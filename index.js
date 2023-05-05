114a1a7e57fd4d5c77ccc31cfbd3d91abac53ece
Sasha Nikulina <92617327+code-7878@users.noreply.github.com>
2023-05-05 20:28:23 +0500

    05.05.2023
    commit for lesson wd

---
+++ index.js
@@ -0,0 +1,10 @@
+
+
+
+
+
+
+var a = [ 4, 7, 2, 9, 5]
+for (var i = 0, sum = 0, b = a.length; i < b; i++) 
+    sum += a[i];
+console.log(sum/b)
---
+++ index1.js
@@ -0,0 +1,9 @@
+for (var i = 1, sum = 0; i <= 7;  i++) {
+
+    const prompt = require("prompt-sync")();
+    const input = prompt(`Введите количество конфет в ${i} день: `);
+    sum = sum + eval(input)
+
+}
+console.log(`Принцесса ест в неделю ${sum} конфет`)
+
